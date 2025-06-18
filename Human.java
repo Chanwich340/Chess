26fc82d08b955622a7f74cacdd0bf41aa0482549
Daniel Franco <danielfplua@gmail.com>
2025-06-17 22:26:30 -0400

    hello!

--- Human.java
+++ Human.java
@@ -37,52 +37,71 @@ public class Human implements Player {
         // CASE 1: PAWN MOVE
 
         // 1a. Check if there are only 2 characters
-
+        if (chessAnswer.length() == 2) {
 
             // 1b. If so, record the last 2 characters as last2chars
-
+            last2chars = chessAnswer;
             // 1c. Find all the Pawns on the Board 
+
             //--> loop through the 2Darray to find the desired Piece object, if you call a Piece object's toString() method, the first character should be what you're looking for
             for(int r=0; r < 8; r++){
                 for(int c=0; c<8; c++){
                     Piece p = pieces[r][c];
+                    if (p != null && p.toString().charAt(0) == 'P' && p.getSide() == side) {
 
-                    // 1d. When you find the desired Piece (P), check if going from their current position to the end position is a Valid Move
-                    // --> use isValid() method in this class
+                        // 1d. When you find the desired Piece (P), check if going from their current position to the end position is a Valid Move
+                        // --> use isValid() method in this class
+                        String startPosition = "" + (char)('a' + c) + (8 - r);
+                        String endPosition = last2chars;
 
-                    
                         // 1e. If you find a validMove, store the startingPosition as first2chars 
-                
-
+                        if (isValid(startPosition + endPosition)) {
+                            first2chars = startPosition;
+                            break;
+                        }
+                    }
                 }
             }
-
+        }       
 
         // CASE 2: FACE PIECE MOVES (Example: Nf3, Knight moves to f3)
+        else {
 
-        // 2a. Record the piece moving (example N) 
-        // --> use substring on 1st character
+            // 2a. Record the piece moving (example N) 
+            // --> use substring on 1st character
+            String pieceMoving = chessAnswer.substring(0, 1);
 
+            // 2b. Record the endLocation of the piece 
+            // --> use substring on last 2 characters & save to last2chars variable
+            last2chars = chessAnswer.substring(chessAnswer.length() - 2);
 
-        // 2b. Record the endLocation of the piece 
-        // --> use substring on last 2 characters & save to last2chars variable
+            // 2c. Find all the Knight Pieces on the Board (g1 and b1) 
+            //--> loop through the 2Darray to find the desired Piece object, if you call a Piece object's toString() method, the first character should be what you're looking for
+            for(int r=0; r < 8; r++){
+                for(int c=0; c<8; c++){
+                    Piece p = pieces[r][c];
+                    if (p != null && p.toString().charAt(0) == pieceMoving.charAt(0) && p.getSide() == side) {
 
+                        // 2d. When you find the desired Piece (N), check if going from their current position to the end position is a Valid Move
+                        // --> use isValid() method in this class
+                        String startPosition = "" + (char)('a' + c) + (8 - r);
+                        String endPosition = last2chars;
 
-
-        // 2c. Find all the Knight Pieces on the Board (g1 and b1) 
-        //--> loop through the 2Darray to find the desired Piece object, if you call a Piece object's toString() method, the first character should be what you're looking for
-        
-
-        // 2d. When you find the desired Piece (N), check if going from their current position to the end position is a Valid Move
-        // --> use isValid() method in this class
-
-
-
-        // 2e. If you find a validMove, store the startingPosition as first2chars 
+                        // 2e. If you find a validMove, store the startingPosition as first2chars 
+                        if (isValid(startPosition + endPosition)) {
+                            first2chars = startPosition;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
 
         // CASE 3: CAPTURES (Is there an x in the middle?)
-
+        if (chessAnswer.contains("x"))  {
+            chessAnswer = chessAnswer.replace("x", "");
+        }
 
 
 
